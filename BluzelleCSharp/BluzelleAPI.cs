4a4b4f76b90be8cb59b83148e866f39249c70ce5
BardinPetr <bardin.petr@gmail.com>
2020-05-28 14:13:17 +0300

    Some documentation added to BluzelleAPI.cs

--- BluzelleCSharp/BluzelleAPI.cs
+++ BluzelleCSharp/BluzelleAPI.cs
@@ -9,10 +9,24 @@ using Newtonsoft.Json.Linq;
 
 namespace BluzelleCSharp
 {
+    /**
+     * <summary>
+     * API for bluzelle database.
+     * Most of operations are available in transaction (methods have 'tx' prefix) and query styles
+     * </summary>
+     */
     public class BluzelleApi : Cosmos
     {
         public const int BlockTimeInSeconds = 5;
 
+        /**
+         * <summary>Initializes Bluzelle Database API</summary>
+         * <param name="namespaceId">Bluzelle Database Namespace ID</param>
+         * <param name="mnemonic">Mnemonic for account in BIP39</param>
+         * <param name="address">Account address in Cosmos format. It'll be verified over given mnemonic</param>
+         * <param name="chainId">Database chain ID. For Bluzelle network it equals to "bluzelle"</param>
+         * <param name="endpoint">REST API endpoint including protocol and port</param>
+         */
         public BluzelleApi(string namespaceId, string mnemonic, string address, string chainId = "bluzelle",
             string endpoint = "http://testnet.public.bluzelle.com:1317") : base(namespaceId, mnemonic, address, chainId,
             endpoint)
@@ -21,6 +35,12 @@ namespace BluzelleCSharp
 
         #region REST API Queries
 
+        /**
+         * <summary>Read value of key <paramref name="id"/></summary>
+         * <param name="id">DB key string</param>
+         * <param name="prove">Use "pread" of "read" operation</param>
+         * <returns>String value</returns>
+         */
         public async Task<string> Read(string id, bool prove = false)
         {
             var data = await Query<JObject>(
@@ -29,11 +49,20 @@ namespace BluzelleCSharp
             return (string) data["value"];
         }
 
+        /**
+         * <summary>Check existence of key <paramref name="id"/></summary>
+         * <param name="id">DB key string</param>
+         * <returns>true if key exists in current namespace</returns>
+         */
         public async Task<bool> HasKey(string id)
         {
             return (bool) (await Query<JObject>($"{CrudServicePrefix}/has/{NamespaceId}/{id}"))["has"];
         }
 
+        /**
+         * <summary>Retrieve list of keys in current namespace</summary>
+         * <returns>List of strings with keys available</returns>
+         */
         public async Task<List<string>> Keys()
         {
             return (await Query<JObject>($"{CrudServicePrefix}/keys/{NamespaceId}"))
@@ -41,48 +70,40 @@ namespace BluzelleCSharp
                 ?.ToObject<List<string>>();
         }
 
+        /**
+         * <summary>Get keys count in current namespace</summary>
+         */
         public async Task<int> Count()
         {
             return (int) (await Query<JObject>($"{CrudServicePrefix}/count/{NamespaceId}"))["count"];
         }
-
-        private Dictionary<string, string> PostprocessKeyVal(JToken data)
-        {
-            return data.Aggregate(
-                new Dictionary<string, string>(),
-                (cur, next) =>
-                {
-                    cur.Add(((string) next["key"])!, (string) next["value"]);
-                    return cur;
-                });
-        }
-
+        
+        /**
+         * <summary>Get all data in Key-Value style from this namespace</summary>
+         * <returns>Dictionary contains keys and values</returns>
+         */
         public async Task<Dictionary<string, string>> GetKeyValues()
         {
             var res = await Query<JObject>($"{CrudServicePrefix}/keyvalues/{NamespaceId}");
             return PostprocessKeyVal(res["keyvalues"] ?? throw new Exception("Failed to get KV list"));
         }
 
+        /**
+         * <summary>Get lease time of key <paramref name="key"/></summary>
+         * <param name="key">DB key string</param>
+         * <returns>Lease time in seconds</returns>
+         */
         public async Task<int> GetLease(string key)
         {
             return (int) (await Query<JObject>($"{CrudServicePrefix}/getlease/{NamespaceId}/{key}"))["lease"]
                    * BlockTimeInSeconds;
         }
 
-        private List<KeyValuePair<string, int>> PostprocessNShortestLeases(JToken data)
-        {
-            return data.Aggregate(
-                new List<KeyValuePair<string, int>>(),
-                (cur, next) =>
-                {
-                    cur.Add(new KeyValuePair<string, int>(
-                        (string) next["key"],
-                        (int) next["lease"] * BlockTimeInSeconds)
-                    );
-                    return cur;
-                });
-        }
-
+        /**
+         * <summary>Get N shortest leases in current namespace</summary>
+         * <param name="n">Leases to return</param>
+         * <returns>List containing shortest leases in ascending order with KeyValuePair of keys and its' leases in seconds</returns>
+         */
         public async Task<List<KeyValuePair<string, int>>> GetNShortestLease(int n)
         {
             var res = await Query<JObject>($"{CrudServicePrefix}/getnshortestleases/{NamespaceId}/{n}");
@@ -230,6 +251,44 @@ namespace BluzelleCSharp
                 ["N"] = $"{n}"
             }, "post", "getnshortestleases", gasInfo);
             return PostprocessNShortestLeases(res["keyleases"] ?? throw new Exception("Failed to get leases list"));
+        }
+
+        #endregion
+
+        #region Utilitary functions
+        /**
+         * <summary>Function for converting JSON response of KeyValue command into C# native dictionary</summary>
+         * <param name="data">JSON array of objects with "key" and "value" fields</param>
+         * <returns>Dictionary contains keys and values</returns>
+         */
+        private static Dictionary<string, string> PostprocessKeyVal(JToken data)
+        {
+            return data.Aggregate(
+                new Dictionary<string, string>(),
+                (cur, next) =>
+                {
+                    cur.Add(((string) next["key"])!, (string) next["value"]);
+                    return cur;
+                });
+        }
+        
+        /**
+         * <summary>Function for converting JSON response of N-shortest-leases command into C# native list with KeyValues</summary>
+         * <param name="data">JSON array of objects with "key" and "lease" fields</param>
+         * <returns>List containing shortest leases in ascending order with KeyValuePair of keys and its' leases in seconds</returns>
+         */
+        private static List<KeyValuePair<string, int>> PostprocessNShortestLeases(JToken data)
+        {
+            return data.Aggregate(
+                new List<KeyValuePair<string, int>>(),
+                (cur, next) =>
+                {
+                    cur.Add(new KeyValuePair<string, int>(
+                        (string) next["key"],
+                        (int) next["lease"] * BlockTimeInSeconds)
+                    );
+                    return cur;
+                });
         }
 
         #endregion
